6f1b3417fb301541bc0062be464e723ece566e94
Ivan Shcheklein <shcheklein@gmail.com>
2025-06-16 12:11:38 -0700

    add trainining of multiple models

---
+++ src/train.py
@@ -0,0 +1,34 @@
+import sys
+from pathlib import Path
+
+from dvclive import Live
+
+if len(sys.argv) != 3:
+    print("Expects model name and model data path")
+    print("train.py model1 data/model1")
+
+data_path = sys.argv[2]
+model_name = sys.argv[1]
+
+
+with Live(dir=Path("dvclive") / model_name) as live:
+    with open(data_path) as f:
+        print(f"Reading data from {data_path}")
+        data = f.read()
+
+    model_path = Path("models") / model_name
+
+    with open(model_path, "w") as m:
+        print(f"Writing data to {model_name}")
+        m.write(data)
+
+    live.log_metric(f"{model_name}/train/loss", 0.1, plot=False)
+
+    live.log_artifact(
+        model_path,
+        type="model",
+        name=model_name,
+        desc=f"Model description for {model_name}",
+        labels=['xgboost']
+    )
+
